2876771e6560275ba9e96629cd66602b229fd168
hninah <hninah03@gmail.com>
2024-10-21 11:45:12 -0600

    First test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -21,33 +21,17 @@ public class CustomListTest {
         return list;
     }
     /**
-     * this gets size of the list
-     * @return
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size
+     plus one
      */
-    public int getCount(){
-        return cities.size();
-    }
-    /**
-     * this adds a city object to the list
-     *for the first phase it will be
-     empty * @param city
-     */
-    public void addCity(City city){
-        @Test
-        public void addCityTest(){
-            list = MockCityList();
-            int listSize = list.getCount();
-            list.addCity(new City("Estevan", "SK"));
-            assertEquals(list.getCount(),listSize + 1);
-        }
-    }
-    /**
-     * this adds a city object to the list
-     *the second phase, you can add the
-     city * @param city
-     */
-    public void addCity(City city){
-        cities.add(city);
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
     }
 
 }
